a0f1cf2e2e1f1589208e4fd91ff6eb338f0cb0f5
Yegor <yegorzhukovskiya@gmail.com>
2022-03-28 21:36:04 +0300

    Navbar done

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import '../../styles/index.scss';
+import Navbar from '../Structure/Navbar';
 
 const App: React.FC = () => (
     <div className="app">
-        <h1>Hello Simple React App!</h1>
+        <Navbar />
     </div>
 );
 
---
+++ src/components/structure/Navbar/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BsQuestionCircle } from "react-icons/bs";
+import './index.scss';
+
+const Navbar: React.FC = () => (
+    <nav className="navbar">
+        <a href="/guide" className="navbar__icon"><BsQuestionCircle size="28px" color="#F0F0F0" /><span className="navbar__icon-underline"></span></a>
+        <h2 className="navbar__title"><a href="/">Krarktosser</a><span className="navbar__title-underline"></span></h2>
+    </nav>
+);
+
+export default Navbar;
