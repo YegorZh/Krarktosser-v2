4706cd92da9b7cba2430d190e3ddee52763ef2b4
Yegor <yegorzhukovskiya@gmail.com>
2022-03-31 22:23:35 +0300

    Last effort to fix height safari bug

--- src/index.tsx
+++ src/index.tsx
@@ -2,4 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App'
 
+const appHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty('--app-height', `${window.innerHeight}px`);
+}
+window.addEventListener('resize', appHeight);
+appHeight();
 ReactDOM.render(<App />, document.getElementById('root'));
